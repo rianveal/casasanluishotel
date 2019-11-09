1c675f8ca1245e58f38ee2ca245dd2bc4e76bf88
Ricardo Velasquez <ricardo.velasquez@GANAYA.LOCAL>
2019-11-09 13:54:31 -0500

    Se administran estilos propios mediantes stylus, proceso en versión movil funcional

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+  $('#reservar').on('click', function(){
+    console.log('....')
+    if( $('.overlay-reserva').is(':hidden') ){
+      $('.overlay-reserva').fadeIn()
+      $('html, body').css('overflowY', 'hidden'); 
+    }else{
+      $('html, body').css('overflowY', 'visible'); 
+    }
+  })
+
+  $('.overlay-reserva--contenedor > .cerrar').on('click', function(){
+    $(this).parent().parent().fadeOut();
+    $('html, body').css('overflowY', 'visible'); 
+  })
+
+  var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+  $('#btnReservar').on('click', function(){
+    if( $('#numeroCedula').val() === ''){
+      mostrarMensaje('NÚMERO DE CEDULA','numeroCedula')
+      return false
+    }else if( $('#nombres').val() === ''){
+      mostrarMensaje('NOMBRES', 'nombres')
+      return false
+    }else if( $('#apellidos').val() === ''){
+      mostrarMensaje('APELLIDOS', 'apellidos')
+      return false
+    }else if( $('#correo').val() === ''){
+      mostrarMensaje('CORREO ELECTRONICO', 'correo')
+      return false
+    }else if( $('#numeroContacto').val() === ''){
+      mostrarMensaje('NÚMERO DE CONTACTO', 'numeroContacto')
+      return false
+    }else if( $('#cantidadPersonas').val() === ''){
+      mostrarMensaje('CANTIDAD DE PERSONAS', 'cantidadPersonas')
+      return false
+    }else if( $('#cantidadHabitaciones').val() === ''){
+      mostrarMensaje('CANTIDAD DE HABITACIONES', 'cantidadHabitaciones')
+      return false
+    }else {
+
+    }
+  })
+})
+
+function mostrarMensaje(campo, valorID){
+  $(".contenedor-mensaje").fadeIn()
+  if( valorID === 'correo' ){
+    if( testEmail.test($('#correo').val()) ){
+
+    }else{
+      console.log('no cumple')
+      return false
+    }
+  }
+  $('#'+valorID).focus();
+  setTimeout(function() {
+    $('#textoMensaje').text('Campo '+campo+' no puede ser vacío.')      
+    $(".contenedor-mensaje").fadeOut(2000);
+  },0);
+}
